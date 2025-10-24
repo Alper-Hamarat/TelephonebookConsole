03ae14a0cfd6e9714a2ce45cd2f2608d5a472e95
Alper Hamarat <alper.hamarat@gmx.de>
2025-10-24 12:40:04 +0200

    Added the office cpp file

---
+++ src/Office.cpp
@@ -0,0 +1,30 @@
+#include "Office.h"
+#include "Entry.h"
+
+Office::Office(string name, string telNr, string address, string designation) : Entry::Entry(name, telNr, address)
+{
+    this->setDesignation(designation);
+}
+
+string Office::getDesignation()
+{
+    return this->designation;
+}
+
+void Office::setDesignation(string newDesignation)
+{
+    if(newDesignation != "")
+    {
+        this->designation = newDesignation;
+    }
+    else
+    {
+        cout <<  "Designation: " << newDesignation <<  " has not been assigned to Office" << endl;
+    }
+}
+
+void Office::printEntry()
+{
+    Entry::printEntry();
+    cout << "Designation: " << this->designation << endl;
+}
