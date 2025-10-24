ff3d4aa88726c28416a21a3bdab3771a3d2f4548
Alper Hamarat <alper.hamarat@gmx.de>
2025-10-24 12:30:28 +0200

    Added Office Header File

---
+++ include/Office.h
@@ -0,0 +1,22 @@
+#ifndef OFFICE_H
+#define OFFICE_H
+
+#include <iostream>
+#include "Entry.h"
+
+using namespace std;
+
+class Office : public Entry
+{
+protected:
+    string designation;
+public:
+    Office(string name, string telNr, string address, string contactPerson);
+    Office();
+    string getDesignation();
+    void setDesignation(string newDesignation);
+
+    void printEntry() override;
+};
+
+#endif /* OFFICE_H */
