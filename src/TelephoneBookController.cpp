2b85d4c8650f0f2d510787a01f55e0eb489dfb84
Alper Hamarat <alper.hamarat@gmx.de>
2025-10-27 19:02:10 +0100

    Implemented the controller functions

--- src/TelephoneBookController.cpp
+++ src/TelephoneBookController.cpp
@@ -2,66 +2,88 @@
 
 #include "UserIO.h"
 
-// Definition der statischen Variable
-//TelephoneBook TelephoneBookController::phoneBook;
-
-TelephoneBookController::TelephoneBookController()
+TelephoneBookController::TelephoneBookController(TelephoneBookView * view , TelephoneBookModel * model)
 {
-    
+    this->view = view;
+    this->model = model;
 }
 
-void TelephoneBookController::listEntries()
+string TelephoneBookController::getUserInput(string textToDisplay)
 {
-    vector<Entry> entries = this->phoneBook.getEntries();
-    //Loop through all entries and call printEntry function
-    vector<Entry>::iterator it;
-    for (it = entries.begin(); it != entries.end(); ++it)
-    {
-        it->printEntry();
-    }
+    cout << textToDisplay << endl;
+    string toReturn = "";
+    cin >> toReturn;
+    return toReturn;
 }
-void TelephoneBookController::listEntry()
+
+char TelephoneBookController::getMenuInput(string textToDisplay)
 {
-    //Get name of entry
-    cout << "Please enter Username" << endl;
-    string username = getUserInput();
-    //get Entry of NAme
-    optional<Entry> entry = this->phoneBook.getEntry(username);
-    if(entry != nullopt)
-    {
-        entry->printEntry();
-    }
-    else
-    {
-        cout << "No entry found" << endl;
-    }
+    cout << textToDisplay << endl;
+    char toReturn = ' ';
+    cin >> toReturn;
+    return toReturn;
 }
-void TelephoneBookController::addEntry()
+
+void TelephoneBookController::run()
 {
-    cout << "Please enter a new Username" << endl;
-    string username = getUserInput();
-    //Check if existing Entry exists
-    optional<Entry> entry = this->phoneBook.getEntry(username);
-    if(entry != nullopt)
+    char menuInput = '0';
+    while(menuInput != '9')
     {
-        cout << "Entry already exists" << endl;
-    }
-    cout << "Please enter a new Telephone Number" << endl;
-    string telNr = getUserInput();
-    this->phoneBook.addEntry(username, telNr);
-}
-void TelephoneBookController::removeEntry()
-{
-    //Get name of entry
-    cout << "Please enter Username" << endl;
-    string username = getUserInput();
-    //get Entry of NAme
-    if(this->phoneBook.removeEntry(username))
-    {
-        cout << "User: " << username << "has been deleted successfully" << endl;
-    }
-    else
-    {
-        cout << "User: " << username << "has not been found" << endl;
+        //Show menu from view
+        this->view->showMenu();
+        //Get Input from user which actions to perform
+        menuInput = this->getMenuInput("Which action would you like to perform?");
+        //Switch based on user input
+        /*
+        void TelephoneBookView::showMenu()
+        {
+            cout << "Welcome to the Telephone Book: " << endl;
+            cout << "To navigate press the Key on the Keyboard and press Enter." << endl;
+            cout << "1: Show Telephone book" << endl;
+            cout << "2: Get an entry from the Telephone book" << endl;
+            cout << "3: Add an entry in the Telephone book" << endl;
+            cout << "4: Remove an entry in the Telephone book" << endl;
+            cout << "5: Edit an entry in the Telephone book" << endl;
+            cout << "9: Exit the Program" << endl;
+        } */
+
+        switch (menuInput)
+        {
+        case '1':
+        {
+            vector<Entry> entries = this->model->getEntries();
+            /* Print the Telephone Book */
+            for(auto i = entries.begin(); i < entries.end(); i++)
+            {
+                i->printEntry();
+            }
+            break;
+        }
+        case '2':
+        {
+            string nameToGet = this->getUserInput("Please enter the name of the user");
+            this->model->getEntry(nameToGet);
+            break;
+        }
+        case '3':
+        {
+            string nameToAdd = this->getUserInput("Please enter the name of the user to add");
+            string telNrToAdd = this->getUserInput("Please enter the Telephone number of the user to add");
+            string addressToAdd = this->getUserInput("Please enter the Address of the user to add");
+
+            Entry newEntry(nameToAdd, telNrToAdd, addressToAdd);
+
+            this->model->addEntry(newEntry);
+            break;
+        }
+        case '4':
+        {
+            string nameToRemoved = this->getUserInput("Please enter the name of the user to remove");
+            this->model->removeEntry(nameToRemoved);
+            break;
+        }
+        default:
+            break;
+        }
     }
 }
